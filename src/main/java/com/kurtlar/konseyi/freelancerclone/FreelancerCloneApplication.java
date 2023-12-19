5d3ca1ad293d55e9733349180dd43d43e2a1bde2
furkan <syrong06@gmail.com>
2023-12-19 15:28:58 +0300

    first commit

---
+++ src/main/java/com/kurtlar/konseyi/freelancerclone/FreelancerCloneApplication.java
@@ -0,0 +1,13 @@
+package com.kurtlar.konseyi.freelancerclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurtlar/konseyi/freelancerclone/FreelancerCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurtlar.konseyi.freelancerclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
