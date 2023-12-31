20e462ff5530737d1cbcad69eb30add031a68d12
furkan <syrong06@gmail.com>
2023-12-31 12:19:08 +0300

    Method level security tüm controller'lara eklendi

--- src/main/java/com/kurtlar/konseyi/freelancerclone/domain/controller/JobController.java
+++ src/main/java/com/kurtlar/konseyi/freelancerclone/domain/controller/JobController.java
@@ -11,6 +11,7 @@ import com.kurtlar.konseyi.freelancerclone.library.rest.Response;
 import com.kurtlar.konseyi.freelancerclone.library.utils.Constants;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
@@ -25,11 +26,13 @@ public class JobController extends BaseController {
     private final JobService service;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<JobResponse> getJobById(@PathVariable String id) {
         return respond(JobMapper.toResponse(service.getById(id)));
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<PageResponse<JobResponse>> getAllJobs(
             @RequestParam(value = "pageNumber", defaultValue = Constants.DEFAULT_PAGE_NUMBER, required = false) String pageNumber,
             @RequestParam(value = "pageSize", defaultValue = Constants.DEFAULT_PAGE_SIZE, required = false) String pageSize,
@@ -39,7 +42,8 @@ public class JobController extends BaseController {
         return respond(service.getAllJobs(pageNumber, pageSize, sortBy, sortDir).map(JobMapper::toResponse));
     }
 
-    @GetMapping("/jobs/owner/{ownerId}")
+    @GetMapping("/owner/{ownerId}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<PageResponse<JobResponse>> getJobsByOwner(
             @PathVariable String ownerId,
             @RequestParam(value = "pageNumber", defaultValue = Constants.DEFAULT_PAGE_NUMBER, required = false) String pageNumber,
@@ -51,7 +55,8 @@ public class JobController extends BaseController {
         return respond(service.getAllJobsByOwner(ownerId, pageNumber, pageSize, sortBy, sortDir).map(JobMapper::toResponse));
     }
 
-    @GetMapping("/jobs/worker/{workerId}")
+    @GetMapping("/worker/{workerId}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<PageResponse<JobResponse>> getJobsByWorker(
             @PathVariable String workerId,
             @RequestParam(value = "pageNumber", defaultValue = Constants.DEFAULT_PAGE_NUMBER, required = false) String pageNumber,
@@ -64,17 +69,20 @@ public class JobController extends BaseController {
 
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN')")
     public Response<JobResponse> createJob(@Valid @RequestBody JobRequest request) {
         return respond(JobMapper.toResponse(service.save(JobMapper.toDto(request))));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN')")
     public Response<JobResponse> updateJob(@PathVariable String id, @RequestBody JobRequest request) {
         return respond(JobMapper.toResponse(service.update(id, JobMapper.toDto(request))));
     }
 
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN')")
     public Response<Void> deleteJob(@PathVariable String id) {
         service.delete(id);
         return new Response<>(MetaResponse.success());
--- src/main/java/com/kurtlar/konseyi/freelancerclone/domain/controller/OfferController.java
+++ src/main/java/com/kurtlar/konseyi/freelancerclone/domain/controller/OfferController.java
@@ -9,6 +9,7 @@ import com.kurtlar.konseyi.freelancerclone.library.rest.DataResponse;
 import com.kurtlar.konseyi.freelancerclone.library.rest.MetaResponse;
 import com.kurtlar.konseyi.freelancerclone.library.rest.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
@@ -22,31 +23,37 @@ public class OfferController extends BaseController {
     private final OfferService service;
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<OfferResponse> createOffer(@RequestBody OfferRequest request) {
         return respond(OfferMapper.toResponse(service.save(OfferMapper.toDto(request))));
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<OfferResponse> getOfferById(@PathVariable String id) {
         return respond(OfferMapper.toResponse(service.getById(id)));
     }
 
     @GetMapping("{jobId}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<DataResponse<OfferResponse>> getAllOffersByJob(@PathVariable String jobId) {
         return respond(service.getAllOffersByJob(jobId).stream().map(OfferMapper::toResponse).collect(Collectors.toList()));
     }
 
     @GetMapping("{userId}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<DataResponse<OfferResponse>> getAllOffersByUser(@PathVariable String userId) {
         return respond(service.getAllOffersByUser(userId).stream().map(OfferMapper::toResponse).collect(Collectors.toList()));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<OfferResponse> updateOffer(@PathVariable String id, @RequestBody OfferRequest request) {
         return respond(OfferMapper.toResponse(service.update(id, OfferMapper.toDto(request))));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN' , 'USER')")
     public Response<Void> deleteOfferById(@PathVariable String id) {
         service.delete(id);
         return new Response<>(MetaResponse.success());
